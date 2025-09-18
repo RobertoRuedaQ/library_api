57c2d5be0d4cde2cf8edc78d7959a6f66df9ea71
Roberto Rueda <rueda.roberto89@gmail.com>
2025-09-18 15:57:59 -0500

    Log In feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    render json: { error: "Unauthorized" }, status: :unauthorized unless current_user
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+
+    header = request.headers["Authorization"]
+    token  = header.split(" ").last if header.present?
+
+    if token
+      decoded = JsonWebToken.decode(token)
+      @current_user = User.find_by(id: decoded[:user_id]) if decoded
+    else
+      @current_user = nil
+    end
+  rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+    @current_user = nil
+  end
+
+  def authorize!(resource, action)
+    unless current_user&.has_permission?(resource, action)
+      render json: { error: "Forbidden: You don’t have permission for this action" }, status: :forbidden
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,40 +3,39 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      post   "login",  to: "sessions#create"
+      delete "logout", to: "sessions#destroy"
 
-      post 'auth/login', to: 'auth#login'
-      post 'auth/register', to: 'auth#register'
-      
       resources :books do
-        resources :copies, except: [:show]
+        resources :copies, except: [ :show ]
       end
-      
+
       resources :magazines do
-        resources :copies, except: [:show]
+        resources :copies, except: [ :show ]
       end
-      
+
       resources :dvds do
-        resources :copies, except: [:show]
+        resources :copies, except: [ :show ]
       end
-      
-      resources :copies, only: [:show] do
+
+      resources :copies, only: [ :show ] do
         member do
           post :borrow
           patch :return
         end
       end
-      
+
       resources :borrowings do
         member do
           patch :renew
           patch :return
         end
       end
-      
-      resources :users, except: [:new, :edit]
-      
-      get 'search/borrowables', to: 'search#borrowables'
-      get 'search/available_copies', to: 'search#available_copies'
+
+      resources :users, except: [ :new, :edit ]
+
+      get "search/borrowables", to: "search#borrowables"
+      get "search/available_copies", to: "search#available_copies"
     end
   end
 end
